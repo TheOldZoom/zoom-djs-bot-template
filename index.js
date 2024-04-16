0a634a71bf4161549e47f315b0adc0263c60dedd
Zoom <xabou2010@gmail.com>
2024-04-15 20:11:01 -0600

    Moved index.js to root

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const path = require('path');
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent] });
 const { TOKEN } = process.env;
-let { devGuildIds, devUserIds} = require("./config.json");
+let { devGuildIds, devUserIds} = require("./src/config.json");
 (async () => {
 
     client.cooldown = new Collection();
     new CommandKit({
         client,
-        commandsPath: path.join(__dirname, 'commands'),
-        eventsPath: path.join(__dirname, 'events'),
-        validationsPath: path.join(__dirname, 'validations'),
+        commandsPath: path.join(__dirname, '/src/commands'),
+        eventsPath: path.join(__dirname, '/src/events'),
+        validationsPath: path.join(__dirname, '/src/validations'),
         devGuildIds: devGuildIds,
         devUserIds: devUserIds,
         bulkRegister: true,
